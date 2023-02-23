8c828b2ac397611f7b9b91fedf78f0cb8f64ed76
Dries <bester.dries@gmail.com>
2023-02-23 16:47:21 +0200

    created soil gel template

--- header.php
+++ header.php
@@ -23,37 +23,15 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'envirosmartsolutions' ); ?></a>
+	<!-- <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'envirosmartsolutions' ); ?></a> -->
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$envirosmartsolutions_description = get_bloginfo( 'description', 'display' );
-			if ( $envirosmartsolutions_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $envirosmartsolutions_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'envirosmartsolutions' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+	<nav id="site-navigation" class="main-navigation">
+		navigation
+	</nav>
+
 	</header><!-- #masthead -->
+
+	
+	
---
+++ page-home.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package EnviroSmartSolutions
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<div class="site-main__hero">
+			hero
+		</div>
+		<div class="site-main__value">
+			Value Props
+		</div>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
