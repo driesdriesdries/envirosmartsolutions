1a49fd43bd3f1b048ed1911117c19e85082c5c59
unknown <bester.dries@gmail.com>
2023-03-30 16:56:46 +0200

    more card work and also wapped the color variable naming around

--- page-home.php
+++ page-home.php
@@ -18,38 +18,38 @@ get_header();
 	<main id="primary" class="site-main container home-page">
 		<div class="section home-banner">
 			<div class="home-banner__copy">
-				<h1>Empowering Sustainable Living: Eco-Friendly Solutions and Practices for a Greener Future</h1>
-				<p>We are focused on environmentally friendly, sustainable, and smart solutions or technologies. We cover topics such as green living, renewable energy, waste management, sustainable products, and eco-friendly practices, with the goal of promoting a more sustainable lifestyle and helping to preserve the environment.</p>
+				<h1>Empowering Sustainable Living</h1>
+				<p>Eco-Friendly Solutions and Practices for a Greener Future.We are focused on environmentally friendly, sustainable, and smart solutions or technologies. We cover topics such as green living, renewable energy, waste management, sustainable products, and eco-friendly practices, with the goal of promoting a more sustainable lifestyle and helping to preserve the environment.</p>
 			</div>
 			<div class="card-section">
 				
-			
+		
+
 				<div class="single-content-card">
-					<div class="single-content-card__header">
+					<div class="single-content-card__header" style="background-image: url('<?php echo get_theme_file_uri('/images/hero-desktop.png'); ?>'); background-position: center center; background-size: cover;">
 						
 					</div>
 					<div class="single-content-card__body">
 						<h3>card heading</h3>
-						<p>card body copy card body copy card body copy card body copy card body copy card body copy card body copy</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe id maiores quasi reiciendis, ducimus rem placeat tenetur suscipit qui, nostrum cum molestias vero deleniti distinctio, tempore repudiandae reprehenderit labore totam.</p>
 					</div>
 					<div class="single-content-card__cta">
-						<a href="#">CTA</a>
+						<a href="<?php echo site_url('/water-treatment'); ?>" class="primary-button-medium">Learn More</a>
 					</div>
 				</div>
 
 				<div class="single-content-card">
-					<div class="single-content-card__header">
+					<div class="single-content-card__header" style="background-image: url('<?php echo get_theme_file_uri('/images/hero-desktop.png'); ?>'); background-position: center center; background-size: cover;">
 						
 					</div>
 					<div class="single-content-card__body">
 						<h3>card heading</h3>
-						<p>card body copy</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe id maiores quasi reiciendis, ducimus rem placeat tenetur suscipit qui, nostrum cum molestias vero deleniti distinctio, tempore repudiandae reprehenderit labore totam.</p>
 					</div>
 					<div class="single-content-card__cta">
-						<a href="#">CTA</a>
+						<a href="<?php echo site_url('/water-treatment'); ?>" class="primary-button-medium">Learn More</a>
 					</div>
 				</div>
-
 				
 			</div>
 		</div>
