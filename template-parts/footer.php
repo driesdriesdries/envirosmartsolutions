17581f6083013f0b4d4efaded0074c0e1b18faf6
unknown <bester.dries@gmail.com>
2023-03-31 08:53:56 +0200

    added a logo to the footer as well

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,5 +1,6 @@
 <footer class="container footer">
-    <div class="section">
-        <p>Copyright Notice: All content © <?php echo date("Y"); ?> EnviroSmart Solutions unless otherwise stated. All rights reserved.</p>
+    <div class="section section__footer">
+        <a href="<?php echo site_url();?>"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt="EnviroSmart Logo"></a>
+        <p>Copyright Notice: All content © <?php echo date("Y"); ?> EnviroSmart Solutions unless otherwise stated. All rights reserved.</p>   
     </div>
 </footer>
