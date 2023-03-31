8e9fc1ba10c326e6bc8bd1283265b4cc9b93cfb5
unknown <bester.dries@gmail.com>
2023-03-31 13:43:51 +0200

    removed text shadows from header, applied a lighter shadow

--- page-water-treatment.php
+++ page-water-treatment.php
@@ -27,12 +27,12 @@ get_header();
 		
 		<div class="container container__2">
 			<?php  get_template_part( 'template-parts/soil-gel/how-it-works' ); ?>
-			<?php  get_template_part( 'template-parts/soil-gel/testimonial' ); ?>
-			<?php  get_template_part( 'template-parts/soil-gel/applications' ); ?>
+			<?php //  get_template_part( 'template-parts/soil-gel/testimonial' ); ?>
+			<?php // get_template_part( 'template-parts/soil-gel/applications' ); ?>
 		</div>
 
 		<div class="container container__3">
-			<?php  get_template_part( 'template-parts/soil-gel/benefits' ); ?>
+			<?php  // get_template_part( 'template-parts/soil-gel/benefits' ); ?>
 		</div>
 
 		<div class="container container__4">
@@ -42,16 +42,16 @@ get_header();
 				<?php 
 					$items = array(
 						array(
-							'heading' => 'x',
-							'content' => 'y'
+							'heading' => 'How does water treatment help remove harmful contaminants from drinking water?',
+							'content' => 'Water treatment is designed to remove harmful contaminants from drinking water by using physical, chemical, or biological methods to purify the water. Contaminants can include bacteria, viruses, chemicals, and other harmful substances. Treatment methods may include filtration, chlorination, ozonation, and ultraviolet (UV) radiation to remove these contaminants and make the water safe for consumption.'
 						),
 						array(
-							'heading' => 'x',
-							'content' => 'y'
+							'heading' => 'What types of water treatment systems are available for home use?',
+							'content' => 'There are many types of water treatment systems available for home use, depending on the specific needs of the household. These include point-of-entry systems, which treat all water entering the home, and point-of-use systems, which treat water at a specific location, such as a faucet or showerhead. Popular systems for home use include activated carbon filters, reverse osmosis systems, and UV purification systems.'
 						),
 						array(
-							'heading' => 'x',
-							'content' => 'y'
+							'heading' => 'Is it necessary to treat well water, or is it already safe for consumption?',
+							'content' => 'Whether well water needs to be treated depends on a number of factors, including the quality of the water and the presence of contaminants. While some well water may be safe for consumption, it is important to have the water tested regularly to ensure that it is free from harmful substances. If testing reveals that the water is contaminated, treatment may be necessary to remove the contaminants and make the water safe to drink.'
 						),
 						// add more items as needed...
 					);
@@ -60,8 +60,8 @@ get_header();
 				?>	
 			</div>
 			<?php 
-				$countries = 'Whatever proof';
-				echo generate_social_proof($countries);				
+				//$countries = 'Whatever proof';
+				// echo generate_social_proof($countries);				
 			?>
 		</div>
 	</main><!-- #main -->
