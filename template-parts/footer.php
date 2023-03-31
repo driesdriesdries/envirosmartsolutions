4e49ab4d64c8ab43dfaddec28d081eb7f46e9e82
unknown <bester.dries@gmail.com>
2023-03-31 10:17:45 +0200

    replaced with svg logo

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -1,6 +1,6 @@
 <footer class="container footer">
     <div class="section section__footer">
-        <a href="<?php echo site_url();?>"><img src="<?php echo get_theme_file_uri('images/logo.png'); ?>" alt="EnviroSmart Logo"></a>
+        <a href="<?php echo site_url();?>"><img src="<?php echo get_theme_file_uri('images/logo.svg'); ?>" alt="EnviroSmart Logo"></a>
         <p>Copyright Notice: All content © <?php echo date("Y"); ?> EnviroSmart Solutions unless otherwise stated. All rights reserved.</p>   
     </div>
 </footer>
