1935c5ccd691cd25e4749c243d5e728136f6bdea
unknown <bester.dries@gmail.com>
2023-04-01 08:55:31 +0200

    added google analytics

--- header.php
+++ header.php
@@ -22,6 +22,15 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet">
 	<link rel="shortcut icon" type="image/x-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-6ZBPEDDCVH"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-6ZBPEDDCVH');
+	</script>
 	<?php wp_head(); ?>
 </head>
 
