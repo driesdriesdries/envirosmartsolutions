a9b69463a0b1b14c394ffcbe68b47753f6412642
unknown <bester.dries@gmail.com>
2023-04-04 16:31:55 +0200

    brought the tags and post navigation inside the article container

--- single.php
+++ single.php
@@ -60,53 +60,55 @@ get_header();
 						endwhile;
 					endif;
 				?>
-			</div>
-			
-			<div class="section section__tag-section">
-				<?php
-					$tags = get_the_tags();
-					if ( $tags ) :
-				?>
-				<div class="tags-list">
-					<span><i>Tagged With:</i></span>
-					<?php foreach ( $tags as $index => $tag ) : ?>
-						<a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" class="tag-link ghost-button ghost-button--small"><?php echo esc_html( $tag->name ); ?></a><?php if ( $index !== count( $tags ) - 1 )  ?>
-					<?php endforeach; ?>
+
+				<div class="section section__tag-section">
+					<?php
+						$tags = get_the_tags();
+						if ( $tags ) :
+					?>
+					<div class="tags-list">
+						<span><i>Tagged With:</i></span>
+						<?php foreach ( $tags as $index => $tag ) : ?>
+							<a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" class="tag-link ghost-button ghost-button--small"><?php echo esc_html( $tag->name ); ?></a><?php if ( $index !== count( $tags ) - 1 )  ?>
+						<?php endforeach; ?>
+					</div>
+						<?php endif; ?>
 				</div>
-					<?php endif; ?>
+			
+				<div class="section section__post-navigation">
+					<?php
+						// Get the current post's category
+						$categories = get_the_category();
+						$category_id = ( ! empty( $categories ) ) ? $categories[0]->term_id : 0;
+
+						// Get the next and previous posts in the same category
+						$next_post = get_next_post( true, '', 'category' );
+						$prev_post = get_previous_post( true, '', 'category' );
+
+						// Output the links
+						if ( $prev_post || $next_post ) {
+							echo '<div class="post-navigation">';
+
+							if ( $prev_post ) {
+								echo '<div class="previous-post-link">';
+								echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $prev_post->ID ) . '">&laquo; ' . esc_html( $prev_post->post_title ) . '</a>';
+								echo '</div>';
+							}
+
+							if ( $next_post ) {
+								echo '<div class="next-post-link">';
+								echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $next_post->ID ) . '">' . esc_html( $next_post->post_title ) . ' &raquo;</a>';
+								echo '</div>';
+							}
+
+							echo '</div>';
+						}
+					?>
+
+				</div>
 			</div>
 			
-			<div class="section section__post-navigation">
-				<?php
-					// Get the current post's category
-					$categories = get_the_category();
-					$category_id = ( ! empty( $categories ) ) ? $categories[0]->term_id : 0;
-
-					// Get the next and previous posts in the same category
-					$next_post = get_next_post( true, '', 'category' );
-					$prev_post = get_previous_post( true, '', 'category' );
-
-					// Output the links
-					if ( $prev_post || $next_post ) {
-						echo '<div class="post-navigation">';
-
-						if ( $prev_post ) {
-							echo '<div class="previous-post-link">';
-							echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $prev_post->ID ) . '">&laquo; ' . esc_html( $prev_post->post_title ) . '</a>';
-							echo '</div>';
-						}
-
-						if ( $next_post ) {
-							echo '<div class="next-post-link">';
-							echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $next_post->ID ) . '">' . esc_html( $next_post->post_title ) . ' &raquo;</a>';
-							echo '</div>';
-						}
-
-						echo '</div>';
-					}
-				?>
-
-			</div>
+			
 
 		</div>
 
