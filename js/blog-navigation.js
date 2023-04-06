7e707ffa3f3850b78b3b84ff57c32265bb749e78
unknown <bester.dries@gmail.com>
2023-04-06 15:20:28 +0200

    started working on the tag dropdown

--- js/blog-navigation.js
+++ js/blog-navigation.js
@@ -1,4 +1,6 @@
 //Blog Navigation 
+
+// CATEGORIES LIST
 // Get the <ul> element
 const categoriesList = document.querySelector('.categories-pane__list--unordered');
 
@@ -7,27 +9,58 @@ const toggleButton = document.querySelector('.categories-pane__toggle--button');
 
 // Add a click event listener to the button
 toggleButton.addEventListener('click', function(event) {
-// Check if the <ul> element has the "expanded" class
-const isExpanded = categoriesList.classList.contains('expanded');
+    // Check if the <ul> element has the "expanded" class
+    const isExpanded = categoriesList.classList.contains('expanded');
 
-// If it has the class, remove it; otherwise, add it
-if (isExpanded) {
-    categoriesList.classList.remove('expanded');
-} else {
-    categoriesList.classList.add('expanded');
-}
+    // If it has the class, remove it; otherwise, add it
+    if (isExpanded) {
+        categoriesList.classList.remove('expanded');
+        toggleButton.classList.remove('expanded');
+    } else {
+        categoriesList.classList.add('expanded');
+        toggleButton.classList.add('expanded');
+    }
 
-// Prevent the click event from bubbling up to the document
-event.stopPropagation();
+    // Prevent the click event from bubbling up to the document
+    event.stopPropagation();
 });
 
 // Add a click event listener to the document
 document.addEventListener('click', function() {
-// Check if the <ul> element has the "expanded" class
-const isExpanded = categoriesList.classList.contains('expanded');
+    // Check if the <ul> element has the "expanded" class
+    const isExpanded = categoriesList.classList.contains('expanded');
 
-// If it has the class, remove it
-if (isExpanded) {
-    categoriesList.classList.remove('expanded');
-}
+    // If it has the class, remove it
+    if (isExpanded) {
+        categoriesList.classList.remove('expanded');
+        toggleButton.classList.remove('expanded');
+    }
+});
+
+
+// TAGS LIST
+// Get the tag dropdown button and panel
+const tagDropdownButton = document.querySelector('.tag-dropdown__button');
+const tagDropdownPanel = document.querySelector('.tag-dropdown__panel');
+
+// Toggle the "expanded" class when the tag dropdown button is clicked
+tagDropdownButton.addEventListener('click', function() {
+  tagDropdownButton.classList.toggle('expanded');
+  tagDropdownPanel.classList.toggle('expanded');
+});
+
+// Add event listener for clicks on the "View Categories" button to remove the "expanded" class
+const categoriesToggleButton = document.querySelector('.categories-pane__toggle--button');
+categoriesToggleButton.addEventListener('click', function() {
+  tagDropdownButton.classList.remove('expanded');
+  tagDropdownPanel.classList.remove('expanded');
+});
+
+// Add event listener for clicks outside the tag dropdown button and panel to remove the "expanded" class
+document.addEventListener('click', function(event) {
+  const isClickInsideTagDropdown = tagDropdownButton.contains(event.target) || tagDropdownPanel.contains(event.target);
+  if (!isClickInsideTagDropdown) {
+    tagDropdownButton.classList.remove('expanded');
+    tagDropdownPanel.classList.remove('expanded');
+  }
 });
--- template-parts/blog-navigation.php
+++ template-parts/blog-navigation.php
@@ -20,15 +20,49 @@
             </div>
             <div class="categories-pane__toggle">
                 <button class="categories-pane__toggle--button">
-                    <span>View Categories ⌄ </span> 
+                    <span>View Categories</span> 
                 </button>
             </div>
         </div>
         
         <div class="tag-dropdown">
-            <button>Tags</button>
+            <button class="tag-dropdown__button"><span>Tags</span></button>
+            <div class="tag-dropdown__panel">
+            <h5>Popular Article Tags</h5>
+                <ul>
+                    <li class="tag-dropdown__panel--link"><a href="#">Water</a></li>
+                    <li class="tag-dropdown__panel--link"><a href="#">Solar</a></li>
+                    <li class="tag-dropdown__panel--link"><a href="#">Wind</a></li>
+                    <li class="tag-dropdown__panel--link"><a href="#">Renewables</a></li>
+                </ul>
+                <hr>
+                <p>View All Tags</p>
+            </div>
+            
+
         </div>
         <div class="search-box">
             <?php get_search_form(); ?>
         </div>
 </div>
+
+<?php /*
+$tags = get_tags(array(
+        'orderby' => 'count',
+        'order' => 'DESC',
+        'number' => 5 // Get only the top 5 tags
+    ));
+
+    if ($tags) {
+        echo '<ul>';
+        foreach ($tags as $tag) {
+            echo '<li><a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a></li>';
+        }
+        echo '</ul>';
+    } else {
+        echo 'No tags found';
+    }
+
+*/
+    
+?>
