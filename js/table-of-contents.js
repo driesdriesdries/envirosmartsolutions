f81cd5be03b234c3fd0c0b9ea5ed54531097c12b
unknown <bester.dries@gmail.com>
2023-04-09 11:33:35 +0200

    added the table of contents sticky functionality

--- js/table-of-contents.js
+++ js/table-of-contents.js
@@ -6,24 +6,74 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Check if any h2 headings are found on the page
     if (h2Headings.length > 0) {
-    // Add an h3 heading with the text "Table of Contents"
-    let tocHeader = "<h3>Table of Contents</h3>";
-    tocDiv.innerHTML = tocHeader;
+      // Add an h3 heading with the text "Table of Contents"
+      let tocHeader = "<h3>Table of Contents</h3>";
+      tocDiv.innerHTML = tocHeader;
 
-    let tocList = "<ul>";
+      let tocList = "<ul>";
 
-    h2Headings.forEach((heading) => {
-      const headingID = heading.innerText.replace(/\s+/g, "-").toLowerCase();
-      heading.setAttribute("id", headingID);
+      h2Headings.forEach((heading) => {
+        const headingID = heading.innerText.replace(/\s+/g, "-").toLowerCase();
+        heading.setAttribute("id", headingID);
 
-      tocList += `<li><a href="#${headingID}">${heading.innerText}</a></li>`;
-    });
+        tocList += `<li><a href="#${headingID}" data-id="${headingID}">${heading.innerText}</a></li>`;
+      });
 
-    tocList += "</ul>";
-    tocDiv.innerHTML += tocList;
+      tocList += "</ul>";
+      tocDiv.innerHTML += tocList;
+
+      // Add event listener for click events on <a> elements
+      tocDiv.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        if (event.target.tagName === "A") {
+          const targetID = event.target.getAttribute("data-id");
+          const targetElement = document.getElementById(targetID);
+
+          if (targetElement) {
+            targetElement.scrollIntoView({
+              behavior: "smooth",
+              block: "start",
+            });
+          }
+        }
+      });
     } else {
-    // Hide the div if there are no h2 headings on the page
-    tocDiv.style.display = "none";
+      // Hide the div if there are no h2 headings on the page
+      tocDiv.style.display = "none";
     }
   }
+});
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const tocDiv = document.querySelector(".table-of-contents");
+  
+    if (tocDiv) {
+      const ulElement = tocDiv.querySelector("ul");
+      const liElements = ulElement ? ulElement.querySelectorAll("li") : [];
+  
+      // Check if the <ul> has any <li> elements
+      if (liElements.length === 0) {
+        // Remove the div with a class of "left" entirely from the page
+        const leftDiv = document.querySelector(".left");
+        if (leftDiv) {
+          leftDiv.parentNode.removeChild(leftDiv);
+        }
+      }
+    }
+  });
+
+  //make the component sticky
+  document.addEventListener("DOMContentLoaded", function () {
+    const tocDiv = document.querySelector(".table-of-contents");
+  
+    if (tocDiv) {
+      window.addEventListener("scroll", function () {
+        if (window.pageYOffset > 500) {
+          tocDiv.classList.add("sticky");
+        } else {
+          tocDiv.classList.remove("sticky");
+        }
+      });
+    }
   });
--- single.php
+++ single.php
@@ -20,97 +20,114 @@ get_header();
 			<div class="single-post-banner" style="background-image: url('<?php echo $image_url; ?>');">
 			</div>	
 
-			<div class="section section__article-body">
-				<?php
-					if (have_posts()) :
-						while (have_posts()) :
-							the_post(); ?>
-							<h1><?php the_title();?></h1>
-							<div class="single-post-meta">
-								<div class="single-post-meta__published-date">
-									<?php
-									// Get the published date
-									$published_date = get_the_date( 'F j, Y' );
 
-									// Display the published date
-									echo '<p>' .'Published on: ' . $published_date . '</p>';
-									?>
-								</div>
-								<div class="single-post-meta__social-icons">
-									<!-- WhatsApp Share Icon -->										
-									<a href="https://api.whatsapp.com/send?text=<?php echo urlencode(get_the_title() . ' - ' . get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
-										<img src="<?php echo get_theme_file_uri('images/social-whatsapp.svg'); ?>" alt="Share on WhatsApp" />
-									</a>
-									<!-- Email Share Icon -->
-									<a href="mailto:?subject=<?php echo rawurlencode(get_the_title()); ?>&amp;body=<?php echo rawurlencode(get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
-										<img src="<?php echo get_theme_file_uri('images/social-mail.svg'); ?>" alt="Share via Email" />
-									</a>
-									<!-- Twitter Share Icon -->
-									<a href="https://twitter.com/intent/tweet?text=<?php echo urlencode(get_the_title() . ' - ' . get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
-										<img src="<?php echo get_theme_file_uri('images/social-twitter.svg'); ?>" alt="Share on Twitter" />
-									</a>
-									<!-- Facebook Share Icon -->
-									<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
-										<img src="<?php echo get_theme_file_uri('images/social-facebook.svg'); ?>" alt="Share on Facebook" />
-									</a>
-								</div>
-
-							</div>
-							<?php  the_content();
-						endwhile;
-					endif;
-				?>
-
-				<div class="section section__tag-section">
-					<?php
-						$tags = get_the_tags();
-						if ( $tags ) :
-					?>
-					<div class="tags-list">
-						<span><i>Tagged With:</i></span>
-						<?php foreach ( $tags as $index => $tag ) : ?>
-							<a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" class="tag-link ghost-button ghost-button--small"><?php echo esc_html( $tag->name ); ?></a><?php if ( $index !== count( $tags ) - 1 )  ?>
-						<?php endforeach; ?>
-					</div>
-						<?php endif; ?>
+			<div class="article-container">
+				<div class="left">
+					<!-- table of contents-->
+					<style>
+						.sticky {
+							position: fixed;
+							top: 10px; /* Adjust this value according to your site's layout */
+							width: 26.85rem;
+							}
+					</style>
+					<div class="table-of-contents"></div>
 				</div>
-			
-				<div class="section section__post-navigation">
+				<div class="right">
+				<div class="section section__article-body">
 					<?php
-						// Get the current post's category
-						$categories = get_the_category();
-						$category_id = ( ! empty( $categories ) ) ? $categories[0]->term_id : 0;
+						if (have_posts()) :
+							while (have_posts()) :
+								the_post(); ?>
+								<h1><?php the_title();?></h1>
+								<div class="single-post-meta">
+									<div class="single-post-meta__published-date">
+										<?php
+										// Get the published date
+										$published_date = get_the_date( 'F j, Y' );
 
-						// Get the next and previous posts in the same category
-						$next_post = get_next_post( true, '', 'category' );
-						$prev_post = get_previous_post( true, '', 'category' );
+										// Display the published date
+										echo '<p>' .'Published on: ' . $published_date . '</p>';
+										?>
+									</div>
+									<div class="single-post-meta__social-icons">
+										<!-- WhatsApp Share Icon -->										
+										<a href="https://api.whatsapp.com/send?text=<?php echo urlencode(get_the_title() . ' - ' . get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
+											<img src="<?php echo get_theme_file_uri('images/social-whatsapp.svg'); ?>" alt="Share on WhatsApp" />
+										</a>
+										<!-- Email Share Icon -->
+										<a href="mailto:?subject=<?php echo rawurlencode(get_the_title()); ?>&amp;body=<?php echo rawurlencode(get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
+											<img src="<?php echo get_theme_file_uri('images/social-mail.svg'); ?>" alt="Share via Email" />
+										</a>
+										<!-- Twitter Share Icon -->
+										<a href="https://twitter.com/intent/tweet?text=<?php echo urlencode(get_the_title() . ' - ' . get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
+											<img src="<?php echo get_theme_file_uri('images/social-twitter.svg'); ?>" alt="Share on Twitter" />
+										</a>
+										<!-- Facebook Share Icon -->
+										<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_the_permalink()); ?>" target="_blank" rel="noopener noreferrer">
+											<img src="<?php echo get_theme_file_uri('images/social-facebook.svg'); ?>" alt="Share on Facebook" />
+										</a>
+									</div>
 
-						// Output the links
-						if ( $prev_post || $next_post ) {
-							echo '<div class="post-navigation">';
-
-							if ( $prev_post ) {
-								echo '<div class="previous-post-link">';
-								echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $prev_post->ID ) . '">&laquo; ' . esc_html( $prev_post->post_title ) . '</a>';
-								echo '</div>';
-							}
-
-							if ( $next_post ) {
-								echo '<div class="next-post-link">';
-								echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $next_post->ID ) . '">' . esc_html( $next_post->post_title ) . ' &raquo;</a>';
-								echo '</div>';
-							}
-
-							echo '</div>';
-						}
+								</div>
+								<?php  the_content();
+							endwhile;
+						endif;
 					?>
 
+					<div class="section section__tag-section">
+						<?php
+							$tags = get_the_tags();
+							if ( $tags ) :
+						?>
+						<div class="tags-list">
+							<span><i>Tagged With:</i></span>
+							<?php foreach ( $tags as $index => $tag ) : ?>
+								<a href="<?php echo esc_url( get_tag_link( $tag->term_id ) ); ?>" class="tag-link ghost-button ghost-button--small"><?php echo esc_html( $tag->name ); ?></a><?php if ( $index !== count( $tags ) - 1 )  ?>
+							<?php endforeach; ?>
+						</div>
+							<?php endif; ?>
+					</div>
+				
+					<div class="section section__post-navigation">
+						<?php
+							// Get the current post's category
+							$categories = get_the_category();
+							$category_id = ( ! empty( $categories ) ) ? $categories[0]->term_id : 0;
+
+							// Get the next and previous posts in the same category
+							$next_post = get_next_post( true, '', 'category' );
+							$prev_post = get_previous_post( true, '', 'category' );
+
+							// Output the links
+							if ( $prev_post || $next_post ) {
+								echo '<div class="post-navigation">';
+
+								if ( $prev_post ) {
+									echo '<div class="previous-post-link">';
+									echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $prev_post->ID ) . '">&laquo; ' . esc_html( $prev_post->post_title ) . '</a>';
+									echo '</div>';
+								}
+
+								if ( $next_post ) {
+									echo '<div class="next-post-link">';
+									echo '<a class="ghost-button ghost-button--small" href="' . get_permalink( $next_post->ID ) . '">' . esc_html( $next_post->post_title ) . ' &raquo;</a>';
+									echo '</div>';
+								}
+
+								echo '</div>';
+							}
+						?>
+
+					</div>
+				</div>
 				</div>
 			</div>
+
 			
 			
-			<!-- table of contents-->
-			<div class="table-of-contents"></div>
+			
+			
 			
 		</div>
 
