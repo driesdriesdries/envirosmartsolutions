fbdc226c9e8e1a42cb5f472caf535a65f9f3d1ae
unknown <bester.dries@gmail.com>
2023-04-11 16:19:55 +0200

    started building a shop

--- template-parts/header.php
+++ template-parts/header.php
@@ -10,9 +10,17 @@
         <div class="nav-right">
             <div class="menu-box">
                 <ul class="menu-box-list">
-                    <li class="menu-box-list--item"><a href="<?php echo site_url('/category/agriculture/'); ?>">Agriculture</a></li>
-                    <li class="menu-box-list--item"><a href="<?php echo site_url('/category/water-treatment'); ?>">Water Treatment</a></li>
+                    <li class="menu-box-list--item"><a href="<?php echo site_url('/shop'); ?>">Shop</a></li>
+                    <li class="menu-box-list--item"><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
+                    <li class="menu-box-list--item"><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
                     <li class="menu-box-list--item modaltrigger"><a href="#">Contact</a></li>
+                    <?php
+                        global $woocommerce;
+
+                        if ($woocommerce->cart->get_cart_contents_count() > 0) {
+                            echo '<li class="menu-box-list--item"><a href="' . wc_get_cart_url() . '" class="view-cart-button">View Cart (' . $woocommerce->cart->get_cart_contents_count() . ')</a></li>';
+                        }
+                    ?>
                 </ul>
             </div>
             <div class="button-box">
