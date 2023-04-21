9900d4eb1e68d66b7420f3393a8b0b929b4cfa5f
unknown <bester.dries@gmail.com>
2023-04-21 16:23:15 +0200

    modified the archive listing to show post excerpts with a charatcer limit

--- archive.php
+++ archive.php
@@ -16,54 +16,51 @@ get_header();
 		<?php my_custom_breadcrumbs(); ?>	
 		<?php get_template_part( 'template-parts/blog-navigation' ); ?>
 		
-		<?php
-			$current_category = get_queried_object();
-			if ( $current_category->description ) {
-				echo '<div class="section section__category-description">';
-				echo '<div class="category-description">' . '<p>' . $current_category->description . '</p>' . '</div>';
-				echo '</div class>';
-			}
-		?>
-		
-		<div class="section card-section">
+		<div class="card-section">
 			<?php
-				$current_category_id = get_queried_object_id();
+			// Get the current category ID
+			$current_category_id = get_queried_object_id();
 
-				$args = array(
-					'post_type'      => 'post',
-					'orderby'        => 'date',
-					'order'          => 'ASC',
-					'posts_per_page' => -1,
-					'cat'            => $current_category_id,
-				);
+			// Set up the query arguments
+			$args = array(
+				'post_type'      => 'post',
+				'orderby'        => 'date',
+				'order'          => 'DESC',
+				'posts_per_page' => -1,
+				'cat'            => $current_category_id,
+			);
 
-				$query = new WP_Query( $args );
+			// Create a new WP_Query instance
+			$query = new WP_Query( $args );
 
-				if ( $query->have_posts() ) :
-					while ( $query->have_posts() ) :
-						$query->the_post();
-						?>
-						<div class="single-content-card">
-							<div class="single-content-card__header" style="background-image: url('<?php echo get_the_post_thumbnail_url(null, 'card-image-thumbnail'); ?>'); background-position: center center; background-size: cover;">
-							</div>
-							<div class="single-content-card__body">
-								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-								<p><?php
-									$description = get_the_category()[0]->description;
-									$trimmed_description = wp_trim_words( $description, 20, '...' );
-									echo $trimmed_description;
-								?></p>
-							</div>
-							<div class="single-content-card__cta">
-								<a href="<?php the_permalink(); ?>" class="primary-button primary-button--medium single-card-cta">Learn More</a>
-							</div>
+			// Start the loop
+			if ( $query->have_posts() ) :
+				while ( $query->have_posts() ) :
+					$query->the_post();
+					?>
+					<div class="single-content-card">
+						<div class="single-content-card__header" style="background-image: url('<?php echo get_the_post_thumbnail_url(null, 'card-image-thumbnail'); ?>'); background-position: center center; background-size: cover;"></div>
+						<div class="single-content-card__body">
+							<h3><a href="<?php the_permalink(); ?>" class="card-link"><?php the_title(); ?></a></h3>
+							<p><?php
+								$excerpt = get_the_excerpt();
+								$trimmed_excerpt = mb_substr($excerpt, 0, 140);
+								if (mb_strlen($excerpt) > 150) {
+									$trimmed_excerpt .= '...';
+								}
+								echo $trimmed_excerpt;
+							?></p>
 						</div>
-						<?php
-					endwhile;
-					wp_reset_postdata();
-				else :
-					echo 'No posts found';
-				endif;
+						<div class="single-content-card__cta">
+							<a href="<?php the_permalink(); ?>" class="primary-button primary-button--medium single-card-cta">Learn More</a>
+						</div>
+					</div>
+					<?php
+				endwhile;
+				wp_reset_postdata();
+			else :
+				echo '<p>No posts found.</p>';
+			endif;
 			?>
 		</div>
 	</div>
--- page-home.php
+++ page-home.php
@@ -22,26 +22,23 @@ get_header();
 				<p>Eco-Friendly Solutions and Practices for a Greener Future.We are focused on environmentally friendly, sustainable, and smart solutions or technologies. We cover topics such as green living, renewable energy, waste management, sustainable products, and eco-friendly practices, with the goal of promoting a more sustainable lifestyle and helping to preserve the environment.</p>
 			</div>
 			<div class="card-section">
-
-			<?php 
-				$categories = get_categories();
-				foreach ($categories as $category) {
-					$image = get_field('category_feature_image', 'category_'.$category->term_id);
-					$image_url = wp_get_attachment_image_src($image['id'], 'card-image-thumbnail')[0];
-					echo '<div class="single-content-card">';
-					echo '<div class="single-content-card__header" style="background-image: url('. $image_url .'); background-position: center center; background-size: cover;"></div>';
-					echo '<div class="single-content-card__body">';
-					echo '<h3><a href="'.get_category_link($category->term_id).'" class="card-link">'.$category->name.'</a></h3>';
-					echo '<p>'. wp_trim_words($category->description, 20) .'</p>';
-					echo '</div>';
-					echo '<div class="single-content-card__cta">';
-					echo '<a href="'.get_category_link($category->term_id).'" class="primary-button primary-button--medium single-card-cta">Learn More</a>';
-					echo '</div>';
-					echo '</div>';
-				}
-			?>
-				
-				
+				<?php 
+					$categories = get_categories();
+					foreach ($categories as $category) {
+						$image = get_field('category_feature_image', 'category_'.$category->term_id);
+						$image_url = wp_get_attachment_image_src($image['id'], 'card-image-thumbnail')[0];
+						echo '<div class="single-content-card">';
+						echo '<div class="single-content-card__header" style="background-image: url('. $image_url .'); background-position: center center; background-size: cover;"></div>';
+						echo '<div class="single-content-card__body">';
+						echo '<h3><a href="'.get_category_link($category->term_id).'" class="card-link">'.$category->name.'</a></h3>';
+						echo '<p>'. wp_trim_words($category->description, 20) .'</p>';
+						echo '</div>';
+						echo '<div class="single-content-card__cta">';
+						echo '<a href="'.get_category_link($category->term_id).'" class="primary-button primary-button--medium single-card-cta">Learn More</a>';
+						echo '</div>';
+						echo '</div>';
+					}
+				?>
 			</div>
 		</div>
 	</main><!-- #main -->
