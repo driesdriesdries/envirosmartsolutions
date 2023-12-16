751115a7357a1e15f8940552a96d594e75818c6b
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2023-12-16 17:28:06 +0200

    changes2pac

--- single.php
+++ single.php
@@ -18,7 +18,7 @@ get_header();
 			$image_url = wp_get_attachment_image_src(get_post_thumbnail_id(), 'banner-image')[0];
 			?>
 			<div class="single-post-banner" style="background-image: url('<?php echo $image_url; ?>');">
-			</div>	
+			</div>
 
 
 			<div class="article-container">
