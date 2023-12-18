5eb97f16ad2a5ed4258b794424429ac6d7ad1a51
Andries Bester <andriesbester@Andriess-MacBook-Air.local>
2023-12-18 15:38:38 +0200

    created the explore section

---
+++ template-parts/explore.php
@@ -0,0 +1,41 @@
+<div class="explore">
+    <h3>Explore More</h3>
+    <?php get_template_part('template-parts/blog-navigation'); ?>
+    <div class="card-group">
+        <?php 
+            $args = array(
+                'number' => 0, // Set to 0 to retrieve all categories
+                'orderby' => 'name',
+                'order' => 'ASC'
+            );
+            $categories = get_categories($args);
+            foreach($categories as $category) {
+                // Generate link to the category
+                $category_link = get_category_link($category->term_id);
+                // Use category name
+                $category_name = $category->name;
+                // Get the ACF image object for the category
+                $category_image = get_field('category_feature_image', 'category_' . $category->term_id);
+                
+                // Check if image object is returned
+                if($category_image) {
+                    $category_image_url = $category_image['url'];
+                    $category_image_alt = $category_image['alt'] ? $category_image['alt'] : $category_name;
+                }
+        ?>
+        <div class="card">
+            <a href="<?php echo esc_url($category_link); ?>" class="card-top">
+                <?php if($category_image): ?>
+                    <img src="<?php echo esc_url($category_image_url); ?>" alt="<?php echo esc_attr($category_image_alt); ?>">
+                <?php else: ?>
+                    <!-- Fallback image or text if no image found -->
+                    <?php echo esc_html($category_name); ?>
+                <?php endif; ?>
+            </a>
+            <div class="card-bottom">
+                <a href="<?php echo esc_url($category_link); ?>"><h4><?php echo esc_html($category_name); ?></h4></a>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
+</div>
